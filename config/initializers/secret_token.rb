4d006e0ab03bb36f9186e859a9373fc0d9c2b15c
Alexandr Kurilin <alex@kurilin.net>
2012-05-17 18:58:59 -0700

    [Infrastructure] Rails project generation
    Generated standard Rails 3.2.3 framework. Updated gemfile to use specific versions of gems by following the
    M.Hartl instrunctions for Rails 3.2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SagerApp::Application.config.secret_token = '357c0862116041485e1d9ed02f0b2b387f2ed3ac361e92bdb58d298efd83d438851dccbc55a1373973d1ebe1e058c9adc21184addfd6f91624b160789f3a85af'
